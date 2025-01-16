26d69f22211ceb8f0324f28c1adc3e1b7ef8f72b
NandoJunioDev <Frnndmaz_cf@outlook.com>
2025-01-16 13:13:44 -0300

    16-05-25

---
+++ JAVA-POO/Lutador/src/Luta.java
@@ -0,0 +1,47 @@
+public class Luta {
+    private Lutador desafiante;
+    private Lutador desafiado;
+    private int rouds;
+    private  boolean aprovada;
+
+    public void marcarLuta(){
+
+    }
+
+
+    public void lutar(){
+
+    }
+
+    public Lutador getDesafiante() {
+        return desafiante;
+    }
+
+    public void setDesafiante(Lutador dd) {
+        this.desafiante = dd;
+    }
+
+    public Lutador getDesafiado() {
+        return desafiado;
+    }
+
+    public void setDesafiado(Lutador dd) {
+        this.desafiado = dd;
+    }
+
+    public int getRouds() {
+        return rouds;
+    }
+
+    public void setRouds(int rouds) {
+        this.rouds = rouds;
+    }
+
+    public boolean isAprovada() {
+        return aprovada;
+    }
+
+    public void setAprovada(boolean aprovada) {
+        this.aprovada = aprovada;
+    }
+}
--- JAVA-POO/Lutador/src/Main.java
+++ JAVA-POO/Lutador/src/Main.java
@@ -8,8 +8,31 @@ public class Main {
                 11,2,3
         );
 
-            lutador[0].apresentar();
+        lutador[1] = new Lutador(
+                "putiscript","Brasil", 29,
+                1.68,37.8,14,2,3
+        );
 
-
+        lutador[2] = new Lutador(
+                "Snapshadow","EUA",35,
+                1.65,80.9,
+                12,2,1
+        );
+        lutador[3] = new Lutador(
+                "Deadcoda","Australia",28,
+                1.93,81.6,
+                13,0,2
+        );
+        lutador[4] = new Lutador(
+                "Ufocobol","Ingraterra",39,
+                1.70,119.3,
+                5,4,3
+        );
+        lutador[5] = new Lutador(
+          "Nerddard", "EUA", 30,
+          1.81,105.7,
+          12,2,4
+        );
+        lutador[5].apresentar();
     }
 }
