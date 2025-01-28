f690cdaf8ad779b1038df289e2b067a0e45b7fe7
NandoJunioDev <frnndmaz_cf@outlook.com>
2025-01-27 21:20:16 -0300

    origin main

--- JAVA-POO/javaSQL/src/Main.java
+++ JAVA-POO/javaSQL/src/Main.java
@@ -1,13 +1,5 @@
-import java.awt.List;
-import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        
     }
-
-
 }
-
-
